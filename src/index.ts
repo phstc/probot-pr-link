a9cf4bfb4e2ecd14291b76bb4578083d8305edaf
Pablo Cantero <pablohstc@gmail.com>
2018-10-20 22:51:58 -0400

    Use :poin_right: instead of PR:

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import { Application } from 'probot'
 import { updateReferencedIssues } from './pullRequestHandler'
 
 export = (app: Application) => {
-  app.on([
-    'pull_request.closed',
-    'pull_request.opened',
-    'pull_request.edited',
-    'pull_request.reopened'
-  ], async context => updateReferencedIssues(context))
+  app.on(
+    [
+      'pull_request.closed',
+      'pull_request.opened',
+      'pull_request.edited',
+      'pull_request.reopened'
+    ],
+    async context => updateReferencedIssues(context)
+  )
 }
